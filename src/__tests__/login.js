977210fc0b15540c7964303cc510b66e16e34e1f
mal-wood <mallorywood413@gmail.com>
2022-10-16 19:14:20 -0400

    clean

--- src/__tests__/login.js
+++ src/__tests__/login.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Login from "../components/Login";
 
 test("onSubmit function is called with username and password", () => {
